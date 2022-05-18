baae9eca65b28bef40a0e2be5e36f40226e6ab3e
k.shimizu <k.shimizu@sonicmoov.com>
2022-05-18 17:18:38 +0900

    second commit

--- NPM/vue-cli-test/src/App.vue
+++ NPM/vue-cli-test/src/App.vue
@@ -1,20 +1,55 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="親から子" @app="child"/>
+  
+  <div :key="index" v-for="(item,index) in items">
+    <CardVue :id="item.id" :title="item.title" :image="item.image" :description="item.description" />
+  </div>
+
+
+
+
 </template>
 
 <script>
 import HelloVue from './components/HelloWorld.vue'
+import CardVue from './components/CardSample.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld:HelloVue
+    HelloWorld:HelloVue,
+    CardVue
   },
 
   methods:{
     child:function(e){
       console.log(e)
+    }
+  },
+
+  data(){
+    return{
+      items:[
+        {
+          id: 1,
+          title: 'タイトル1',
+          image: 'img1.jpg',
+          description: '詳細1'
+        },
+        {
+          id: 2,
+          title: 'タイトル2',
+          image: 'img2.jpg',
+          description: '詳細2'
+        },
+        {
+          id: 3,
+          title: 'タイトル3',
+          image: 'img3.jpg',
+          description: '詳細3'
+        }
+      ]
     }
   }
 
---
+++ NPM/vue-cli-test/src/components/CardSample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{id}} {{title}} {{image}}  {{description}}</div>
+</template>
+
+<script>
+// import { numberLiteralTypeAnnotation } from '@babel/types';
+// import { stringify } from 'querystring';
+
+export default {
+    props: {
+        id: Number,
+        title: String,
+        image: String,
+        description: String
+    }
+
+}
+</script>
+
+<style>
+
+</style>
